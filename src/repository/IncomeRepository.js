f72686650b7621c32bfd7b41b1696a219b97e232
WellsSA <wel.cavzod@gmail.com>
2021-12-22 07:25:19 -0300

    feat: Income repository definition

---
+++ src/repository/IncomeRepository.js
@@ -0,0 +1,17 @@
+import http from 'http';
+
+const API_BASE_URL = 'http://localhost:3000';
+
+class IncomeRepository {
+  async makeRequest(url) {
+    // @TODO: Implement method
+    return null;
+  }
+
+  async getConversions() {
+    // @TODO: Implement method
+    return null;
+  }
+}
+
+export default IncomeRepository;
