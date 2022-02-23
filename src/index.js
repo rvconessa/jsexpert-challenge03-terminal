b1d8a5a6e7f9dc22bb34f4eb789b4856f4bc6094
Renan <rvconessa@gmail.com>
2022-02-23 15:22:01 -0300

    feat: add code to mainLoop

--- src/index.js
+++ src/index.js
@@ -13,11 +13,28 @@ const service = new IncomeService();
 async function mainLoop() {
   console.info('🚀 Running...\n');
   try {
-    // TODO: Looks like you have some work to do right here :)
+    terminal.initializeTable();
+
+    const answer = await terminal.readLine(
+      'Qual seu cargo e pretensão salarial em BRL? (position; expectation)\n Insira: '
+    );
+
+    if(answer === VOCABULARY.STOP) {
+      terminal.closeTerminal();
+      console.info('finishing terminal instance!');
+      return;
+    }
+
+    const response = await service.generateIncomeFromString(answer);
+    terminal.updateData(response.format());
+    terminal.successMessage('Register successfully inserted');
+    
+    return mainLoop();
+    
   } catch (error) {
-    // TODO: Don't forget of handling some errors beautifully ;)
+    terminal.errorMessage(error.message);
+    return mainLoop();
   }
-  return mainLoop();
 }
 
 await mainLoop();
