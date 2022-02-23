5b22f14287626627e84f4e9afe9602241cc3ac25
Renan <rvconessa@gmail.com>
2022-02-23 15:22:20 -0300

    feat: format data

--- src/service/IncomeService.js
+++ src/service/IncomeService.js
@@ -8,9 +8,23 @@ class IncomeService {
 
   async generateIncomeFromString(incomeString, delimiter = ';') {
     const [position, expectation] = incomeString.split(delimiter);
+    if(position === '') {
+      throw new Error('Position is a required field. Please make sure you are providing a position.');
+    }
 
-    // @TODO: Implement method
-    return null;
+    if(isNaN(expectation)) {
+      throw new Error('A valid Expectation is required. Please note that only numbers are allowed.');
+    }
+
+    const data = new Income({ position });
+    data.expectation.value = +expectation;
+
+    const conversions = await this.incomeRepository.getConversions();
+    data.conversion01.value = expectation * conversions[data.conversion01.currency]
+    data.conversion02.value = expectation * conversions[data.conversion02.currency]
+    data.conversion03.value = expectation * conversions[data.conversion03.currency]
+
+    return data;
   }
 }
 
