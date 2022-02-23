e195092da3b2ece11dcfa3d03373ee89e55ea2ba
Renan <rvconessa@gmail.com>
2022-02-23 15:20:39 -0300

    feat: create makeRequest and find results

--- src/repository/IncomeRepository.js
+++ src/repository/IncomeRepository.js
@@ -4,13 +4,24 @@ const API_BASE_URL = 'http://localhost:3000';
 
 class IncomeRepository {
   async makeRequest(url) {
-    // @TODO: Implement method
-    return null;
+    return new Promise((resolve, reject) => {
+      http.get(url, res => {
+        let chunks = [];
+        res.on('data', chunk => {
+            chunks.push(chunk);
+        })
+
+        res.on('end', () => {
+            const data = Buffer.concat(chunks).toString();
+            resolve(JSON.parse(data));
+        })
+      })
+    })
   }
 
   async getConversions() {
-    // @TODO: Implement method
-    return null;
+    const data = await this.makeRequest(`${API_BASE_URL}/convert`);
+    return data.results;
   }
 }
 
